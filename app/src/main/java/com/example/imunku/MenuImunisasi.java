04c45c4978322bd8c28c0c9fa4b6bbffae85e786
Hernanda Dzaki Permana <hernanda03@gmail.com>
2023-11-26 17:44:35 +0700

    comit layout menu & item imunisasi

---
+++ app/src/main/java/com/example/imunku/MenuImunisasi.java
@@ -0,0 +1,14 @@
+package com.example.imunku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuImunisasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_imunisasi);
+    }
+}
